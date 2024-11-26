01b5a730ed5d7c630be280087bd5a93933f2ba92
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-11-26 21:05:38 +0100

    Changed names to some files

--- frontend-event/src/App.jsx
+++ frontend-event/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import EventPage from "./pages/EventPage";
-import UserPage from "./pages/UserPage";
+import EventPage from "./pages/MainEventsPage";
+import UserPage from "./pages/MainUserPage";
 import FriendsAll from "./components/FriendsAll";
 import EditProfile from "./components/EditProfile";
 import { Link } from "react-router-dom";
